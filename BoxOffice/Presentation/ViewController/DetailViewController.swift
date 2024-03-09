a334fcfe7a288a93449aa5c1ed2e9254017e97d7
Effie <effie.thedev@gmail.com>
2024-03-09 13:55:22 +0900

    feat: DetailViewController 화면 구현 및 속성 추가
    - 프로젝트 그룹 정리

---
+++ BoxOffice/Presentation/ViewController/DetailViewController.swift
@@ -0,0 +1,89 @@
+import UIKit
+
+final class DetailViewController: UIViewController {
+  // MARK: Properties
+  
+  private let movieTitle: String
+  
+  private let movieCode: String
+  
+  private let viewModel: DetailViewInput
+  
+  // MARK: Views
+  
+  private let scrollView: UIScrollView = UIScrollView()
+  
+  private let contentStackView: UIStackView = {
+    let stack = UIStackView()
+    stack.axis = .vertical
+    return stack
+  }()
+  
+  // MARK: Initializers
+  
+  init(
+    movieTitle: String,
+    movieCode: String,
+    viewModel: DetailViewInput
+  ) {
+    self.movieTitle = movieTitle
+    self.movieCode = movieCode
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: Lifecycle Methods
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureLayout()
+    self.viewModel.viewDidLoad()
+  }
+}
+
+extension DetailViewController {
+  private func configureLayout() {
+    configureHierachy()
+    configureLayoutStyle()
+    configureConstraint()
+  }
+  
+  private func configureHierachy() {
+    self.view.addSubview(self.scrollView)
+    self.scrollView.addSubview(self.contentStackView)
+  }
+  
+  private func configureLayoutStyle() {
+    self.scrollView.translatesAutoresizingMaskIntoConstraints = false
+    self.contentStackView.translatesAutoresizingMaskIntoConstraints = false
+  }
+  
+  private func configureConstraint() {
+    NSLayoutConstraint.activate([
+      self.scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+      self.scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+      self.scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+      self.scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+      
+      self.contentStackView.leadingAnchor.constraint(equalTo: self.scrollView.contentLayoutGuide.leadingAnchor),
+      self.contentStackView.trailingAnchor.constraint(equalTo: self.scrollView.contentLayoutGuide.trailingAnchor),
+      self.contentStackView.topAnchor.constraint(equalTo: self.scrollView.contentLayoutGuide.topAnchor),
+      self.contentStackView.bottomAnchor.constraint(equalTo: self.scrollView.contentLayoutGuide.bottomAnchor),
+      
+      self.contentStackView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor, multiplier: 1),
+    ])
+    
+    let heightConstraint = self.contentStackView.heightAnchor.constraint(equalToConstant: self.scrollView.frame.height)
+    heightConstraint.priority = .defaultLow
+    heightConstraint.isActive = true
+  }
+}
+
+protocol DetailViewInput {
+  func viewDidLoad()
+}
